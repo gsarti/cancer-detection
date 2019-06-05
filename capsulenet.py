1ec2f1e044b28d44c00837bf1c46af0ed869346f
gsarti <gabriele.sarti996@gmail.com>
2019-06-05 20:48:07 +0200

    Added batch generator for CapsNet

--- capsulenet.py
+++ capsulenet.py
@@ -21,16 +21,18 @@ from keras import layers, models, optimizers
 from keras import backend as K
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
-from utils import combine_images
+from sklearn.model_selection import train_test_split
+from utils import plot_log
 from PIL import Image
 from capsulelayers import CapsuleLayer, PrimaryCap, Length, Mask
+from generator import DataGenerator
 
 K.set_image_data_format('channels_last')
 
 
 def CapsNet(input_shape, n_class, routings):
     """
-    A Capsule Network on MNIST.
+    A Capsule Network on Histopathological Cancer dataset.
     :param input_shape: data shape, 3d, [width, height, channels]
     :param n_class: number of classes
     :param routings: number of routing iterations
@@ -69,12 +71,7 @@ def CapsNet(input_shape, n_class, routings):
     train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
     eval_model = models.Model(x, [out_caps, decoder(masked)])
 
-    # manipulate model
-    noise = layers.Input(shape=(n_class, 16))
-    noised_classcaps = layers.Add()([classcaps, noise])
-    masked_noised_y = Mask()([noised_classcaps, y])
-    manipulate_model = models.Model([x, y, noise], decoder(masked_noised_y))
-    return train_model, eval_model, manipulate_model
+    return train_model, eval_model
 
 
 def margin_loss(y_true, y_pred):
@@ -90,17 +87,15 @@ def margin_loss(y_true, y_pred):
     return K.mean(K.sum(L, 1))
 
 
-def train(model, data, args):
+def train(model, train_gen, val_gen, args):
     """
     Training a CapsuleNet
     :param model: the CapsuleNet model
-    :param data: a tuple containing training and testing data, like `((x_train, y_train), (x_test, y_test))`
+    :param train_gen: a training data generator
+    :param val_gen: a validation data generator
     :param args: arguments
     :return: The trained model
     """
-    # unpacking the data
-    (x_train, y_train), (x_test, y_test) = data
-
     # callbacks
     log = callbacks.CSVLogger(args.save_dir + '/log.csv')
     tb = callbacks.TensorBoard(log_dir=args.save_dir + '/tensorboard-logs',
@@ -115,28 +110,12 @@ def train(model, data, args):
                   loss_weights=[1., args.lam_recon],
                   metrics={'capsnet': 'accuracy'})
 
-    """
-    # Training without data augmentation:
-    model.fit([x_train, y_train], [y_train, x_train], batch_size=args.batch_size, epochs=args.epochs,
-              validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[log, tb, checkpoint, lr_decay])
-    """
-
-    # Begin: Training with data augmentation ---------------------------------------------------------------------#
-    def train_generator(x, y, batch_size, shift_fraction=0.):
-        train_datagen = ImageDataGenerator(width_shift_range=shift_fraction,
-                                           height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
-        generator = train_datagen.flow(x, y, batch_size=batch_size)
-        while 1:
-            x_batch, y_batch = generator.next()
-            yield ([x_batch, y_batch], [y_batch, x_batch])
-
-    # Training with data augmentation. If shift_fraction=0., also no augmentation.
-    model.fit_generator(generator=train_generator(x_train, y_train, args.batch_size, args.shift_fraction),
-                        steps_per_epoch=int(y_train.shape[0] / args.batch_size),
+    model.fit_generator(generator=train_gen,
                         epochs=args.epochs,
-                        validation_data=[[x_test, y_test], [y_test, x_test]],
-                        callbacks=[log, tb, checkpoint, lr_decay])
-    # End: Training with data augmentation -----------------------------------------------------------------------#
+                        validation_data=val_gen,
+                        callbacks=[log, tb, checkpoint, lr_decay],
+                        use_multiprocessing=True,
+                        workers=6)
 
     model.save_weights(args.save_dir + '/trained_model.h5')
     print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
@@ -147,61 +126,31 @@ def train(model, data, args):
     return model
 
 
-def test(model, data, args):
-    x_test, y_test = data
-    y_pred, x_recon = model.predict(x_test, batch_size=100)
-    print('-'*30 + 'Begin: test' + '-'*30)
-    print('Test acc:', np.sum(np.argmax(y_pred, 1) == np.argmax(y_test, 1))/y_test.shape[0])
+def load_generators(train_dir):
+    # Parameters
+    params = {'dim': (96,96),
+              'batch_size': 100,
+              'n_classes': 2,
+              'n_channels': 3,
+              'shuffle': True}
 
-    img = combine_images(np.concatenate([x_test[:50],x_recon[:50]]))
-    image = img * 255
-    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + "/real_and_recon.png")
-    print()
-    print('Reconstructed images are saved to %s/real_and_recon.png' % args.save_dir)
-    print('-' * 30 + 'End: test' + '-' * 30)
-    plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png"))
-    plt.show()
+    # Data
+    data = pd.read_csv(train_dir + 'train_labels.csv')
+    train, val = train_test_split(data['id'], 
+                                  test_size = 0.1,
+                                  random_state = 42,
+                                  shuffle=True)
+    labels = {row['id']:row['label'] for index, row in data.iterrows()}
 
+    # Generators
+    train_gen = DataGenerator(train, labels, train_dir, **params)
+    val_gen = DataGenerator(val, labels, train_dir, **params)
 
-def manipulate_latent(model, data, args):
-    print('-'*30 + 'Begin: manipulate' + '-'*30)
-    x_test, y_test = data
-    index = np.argmax(y_test, 1) == args.cancer
-    number = np.random.randint(low=0, high=sum(index) - 1)
-    x, y = x_test[index][number], y_test[index][number]
-    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
-    noise = np.zeros([1, 10, 16])
-    x_recons = []
-    for dim in range(16):
-        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
-            tmp = np.copy(noise)
-            tmp[:,:,dim] = r
-            x_recon = model.predict([x, y, tmp])
-            x_recons.append(x_recon)
-
-    x_recons = np.concatenate(x_recons)
-
-    img = combine_images(x_recons, height=16)
-    image = img*255
-    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.cancer)
-    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.cancer))
-    print('-' * 30 + 'End: manipulate' + '-' * 30)
-
-
-def load_cancer():
-    with np.load("./data/cancerz.npz") as f:
-        x_train, y_train = f["x_train"], f["y_train"]
-        x_test = f["x_test"]
-    x_train = x_train.reshape(-1, 96, 96, 3).astype('float32') / 255.
-    x_test = x_test.reshape(-1, 96, 96, 3).astype('float32') / 255.
-    y_test = np.zeros(x_test.shape[0]).astype('float32')
-    return (x_train, y_train), (x_test, y_test)
+    return train_gen, val_gen
 
 
 if __name__ == "__main__":
-    import os
-    import argparse
-    from keras.preprocessing.image import ImageDataGenerator
+
     from keras import callbacks
 
     # setting the hyper parameters
@@ -216,15 +165,10 @@ if __name__ == "__main__":
                         help="The coefficient for the loss of decoder")
     parser.add_argument('-r', '--routings', default=3, type=int,
                         help="Number of iterations used in routing algorithm. should > 0")
-    parser.add_argument('--shift_fraction', default=0.1, type=float,
-                        help="Fraction of pixels to shift at most in each direction.")
     parser.add_argument('--debug', action='store_true',
                         help="Save weights by TensorBoard")
-    parser.add_argument('--save_dir', default='./result')
-    parser.add_argument('-t', '--testing', action='store_true',
-                        help="Test the trained model on testing dataset")
-    parser.add_argument('--cancer', default=1, type=int,
-                        help="Class to manipulate")
+    parser.add_argument('--save_dir', default='results')
+    parser.add_argument('--train_dir', default='data/train/')
     parser.add_argument('-w', '--weights', default=None,
                         help="The path of the saved weights. Should be specified when testing")
     args = parser.parse_args()
@@ -233,22 +177,16 @@ if __name__ == "__main__":
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
-    # load data
-    (x_train, y_train), (x_test, y_test) = load_cancer()
+    train_gen, val_gen = load_generators(args.train_dir)
 
     # define model
-    model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
-                                                  n_class=len(np.unique(np.argmax(y_train, 1))),
-                                                  routings=args.routings)
+    model, eval_model = CapsNet(input_shape=train_gen.shape[1:],
+                                n_class=train_gen.n_class,
+                                routings=args.routings)
     model.summary()
 
     # train or test
     if args.weights is not None:  # init the model weights with provided one
         model.load_weights(args.weights)
     if not args.testing:
-        train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
-    else:  # as long as weights are given, will run testing
-        if args.weights is None:
-            print('No weights are provided. Will test using random initialized weights.')
-        manipulate_latent(manipulate_model, (x_test, y_test), args)
-        test(model=eval_model, data=(x_test, y_test), args=args)
+        train(model=model, train_gen=train_gen, val_gen=val_gen, args=args)
